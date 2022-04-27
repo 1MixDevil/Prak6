041971135d6cea2c25d8e99d2a89603b6e557c3c
Misha <antonomisha90@gmail.com>
2022-04-27 19:34:14 +0300

    Metro_all

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-
-
 import asyncio
-import time
 import random
 
 
 class Station_Metro:
-    MAX_PEOPLE = 200
+    MAX_PEOPLE = 400
 
     def __init__(self, name, left=0, right=0, ):
         self.name = name
@@ -17,22 +14,45 @@ class Station_Metro:
 
     async def add_people(self):
         while True:
-            # print(f"На станции {self.name} {self.people} людей")
-            await asyncio.sleep(4)
-            rand_people = random.randint(1, 50)
-            self.people += rand_people
-            self.people_l += (50 - rand_people)
+            await asyncio.sleep(0.01)
+            rand_people = random.randint(0, 1)
+            if self.right == 0:
+                self.people_l += rand_people
+            elif self.left == 0:
+                self.people_r += rand_people
+            else:
+                self.people_r += rand_people
+                self.people_l += (1 - rand_people)
 
-    def del_people(self, has_already):
-        print(f"Поезд стоит на станции {self.name}")
-        add_max = self.MAX_PEOPLE - has_already
-        max = self.people - add_max
-        if max < 0:
-            self.people = 0
-            return max
+    def del_people_r(self, has_people, num):
+        # print(f"На станции {self.name} стоит {self.people_r} которые хотят направо и {self.people_l} налево")
+        free = (self.MAX_PEOPLE - has_people)  # Получение свободных мест в поезде
+        platform_free = self.people_r - free  # Сколько людей останется на платформе, если заполнить все места
+        if platform_free <= 0:
+            people = int(self.people_r)
+            self.people_r = 0
+            # print(f"Поезд {num} забрал {people} людей (ВСЕХ)")
+            return people
         else:
-            self.people -= max
-            return max
+            # print(f"Поезд {num} забрал {free} людей")
+            self.people_r -= free
+            return free
+
+    def del_people_l(self, has_people, num):
+        # print(f"На станции {self.name} стоит {self.people_r} которые хотят направо и {self.people_l} налево")
+        free = self.MAX_PEOPLE - has_people
+        if free < 0:
+            return 0
+        platform_free = self.people_l - free
+        if platform_free <= 0:
+            people = int(self.people_l)
+            self.people_l = 0
+            # print(f"Поезд {num} забрал {people} людей (ВСЕХ)")
+            return people
+        else:
+            # print(f"Поезд {num} забрал {free} людей")
+            self.people_l -= free
+            return free
 
 
 class Train:
@@ -52,6 +72,11 @@ class Train:
         self.position = position
         self.standing = True
 
+    def del_people(self):
+        del_pep = random.randint(0, self.people)
+        self.people -= del_pep
+        # print(f"Из поезда {self.number} вышли {del_pep}")
+
     async def directions(self):
         while True:
             if self.position >= len(stations) - 1:
@@ -60,35 +85,70 @@ class Train:
                 self.right = True
 
             if self.right:
-                print(f"Поезд {self.number} уехал со станции {stations[self.position].name}")
+                # print(f"Поезд {self.number} уехал со станции {stations[self.position].name}")
                 await asyncio.sleep(stations[self.position].right)
-                print(f"Поезд {self.number} прибыл на станцию {stations[self.position + 1].name}")
                 self.position += 1
+                # print(f"Поезд {self.number} прибыл на станцию {stations[self.position].name} \n \n")
+                self.del_people()
+                self.people += stations[self.position].del_people_r(self.people, self.number)
+                if self.position >= len(stations) - 1:
+                    self.people += stations[self.position].del_people_l(self.people, self.number)
+                else:
+                    self.people += stations[self.position].del_people_r(self.people, self.number)
+
             elif not self.right:
-                f"Поезд {self.number} уехал со станции {stations[self.position].name}"
+                # print(f"Поезд {self.number} уехал со станции {stations[self.position].name}")
                 await asyncio.sleep(stations[self.position].left)
-                print(f"Поезд {self.number} прибыл на станцию {stations[self.position - 1].name}")
                 self.position -= 1
-            await asyncio.sleep(5)
+                # print(f"Поезд {self.number} прибыл на станцию {stations[self.position].name} \n \n")
+                self.del_people()
+                if self.position <= 0:
+                    self.people += stations[self.position].del_people_r(self.people, self.number)
+                else:
+                    self.people += stations[self.position].del_people_l(self.people, self.number)
+
+            await asyncio.sleep(0.15)
 
 
 async def Main():
-    for i in range(2):
-        loop.create_task(Train(i, 0, 1).directions())
-        await asyncio.sleep(4)
+    trains = []
+    n = 5
+    for i in range(n):
+        a = Train(i + 1, 0, 1)
+        trains.append(a)
+        loop.create_task(a.directions())
+        if i == 0:
+            loop.create_task(start())
+        await asyncio.sleep(22.8 / n)
+
+    while True:
+        for i in stations:
+            print(f"{i.name}: {int(i.people_r) + int(i.people_l)}")
+
+        print(f"\n \n")
+        for i in trains:
+            try:
+                print(
+                    f"{i.number}: {stations[i.position].name} -> {stations[i.position + 1].name if i.right else stations[i.position - 1].name}")
+            except:
+                print(
+                    f"{i.number}: {stations[i.position].name} -> {stations[i.position - 1].name if i.right else stations[i.position + 1].name}")
+        await asyncio.sleep(10)
+        print("\n")
+
+
+async def start():
+    await asyncio.sleep(10.8)
     for i in range(0, len(stations)):
         loop.create_task(stations[i].add_people())
 
-    while True:
-        await asyncio.sleep(4)
-
 
 stations = [
-    Station_Metro('Rokossovskoy', 0, 6),
-    Station_Metro("Sobornaya", 6, 3),
-    Station_Metro("Crystal", 3, 2),
-    Station_Metro("Dyrochnaya", 2, 7),
-    Station_Metro("Lib_Push", 7, 0),
+    Station_Metro('Rokossovskoy', 0, 3.6),
+    Station_Metro("Sobornaya", 3.6, 1.8),
+    Station_Metro("Crystal", 1.8, 1.2),
+    Station_Metro("Dyrochnaya", 1.2, 4.2),
+    Station_Metro("Lib_Push", 4.2, 0),
 ]
 
 loop = asyncio.get_event_loop()
